087c292040960c0db8c07be01f86d86ea02ccb72
jundayw <jundayw@126.com>
2024-08-12 18:31:18 +0800

    feat:getConfig(?string $option = null)

--- src/ClientDecorator.php
+++ src/ClientDecorator.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\UriTemplate\UriTemplate;
 use Nacosvel\OpenHttp\Contracts\ClientDecoratorInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\UriInterface;
 
 class ClientDecorator implements ClientDecoratorInterface
 {
@@ -20,20 +19,36 @@ class ClientDecorator implements ClientDecoratorInterface
     }
 
     /**
+     * Get a client configuration option.
+     *
+     * These options include default request options of the client, a "handler"
+     * (if utilized by the concrete client), and a "base_uri" if utilized by
+     * the concrete client.
+     *
+     * @param string|null $option The config option to retrieve.
+     *
+     * @return mixed
+     */
+    public function getConfig(?string $option = null): mixed
+    {
+        return $this->client->getConfig($option);
+    }
+
+    /**
      * Create and send an HTTP request.
      *
      * Use an absolute path to override the base path of the client, or a
      * relative path to append to the base path of the client. The URL can
      * contain the query string as well.
      *
-     * @param string              $method  HTTP method.
-     * @param string|UriInterface $uri     URI object or string.
-     * @param array               $options Request options to apply.
+     * @param string $method  HTTP method.
+     * @param string $uri     URI object or string.
+     * @param array  $options Request options to apply.
      *
      * @return ResponseInterface The `Psr\Http\Message\ResponseInterface` instance
      * @throws GuzzleException
      */
-    public function request(string $method, $uri, array $options = []): ResponseInterface
+    public function request(string $method, string $uri = '', array $options = []): ResponseInterface
     {
         return $this->client->request($method, UriTemplate::expand($uri, $options), $options);
     }
@@ -46,13 +61,13 @@ class ClientDecorator implements ClientDecoratorInterface
      * contain the query string as well. Use an array to provide a URL
      * template and additional variables to use in the URL template expansion.
      *
-     * @param string              $method  HTTP method
-     * @param string|UriInterface $uri     URI object or string.
-     * @param array               $options Request options to apply.
+     * @param string $method  HTTP method
+     * @param string $uri     URI object or string.
+     * @param array  $options Request options to apply.
      *
      * @return PromiseInterface The `GuzzleHttp\Promise\PromiseInterface` instance
      */
-    public function requestAsync(string $method, $uri, array $options = []): PromiseInterface
+    public function requestAsync(string $method, string $uri = '', array $options = []): PromiseInterface
     {
         return $this->client->requestAsync($method, UriTemplate::expand($uri, $options), $options);
     }
--- src/Contracts/ClientDecoratorInterface.php
+++ src/Contracts/ClientDecoratorInterface.php
@@ -8,6 +8,19 @@ use Psr\Http\Message\ResponseInterface;
 interface ClientDecoratorInterface
 {
     /**
+     * Get a client configuration option.
+     *
+     * These options include default request options of the client, a "handler"
+     * (if utilized by the concrete client), and a "base_uri" if utilized by
+     * the concrete client.
+     *
+     * @param string|null $option The config option to retrieve.
+     *
+     * @return mixed
+     */
+    public function getConfig(?string $option = null): mixed;
+
+    /**
      * Request the remote $uri by a HTTP $method verb
      *
      * @param string $method  The uri string.
@@ -16,7 +29,7 @@ interface ClientDecoratorInterface
      *
      * @return ResponseInterface The `Psr\Http\Message\ResponseInterface` instance
      */
-    public function request(string $method, string $uri, array $options = []): ResponseInterface;
+    public function request(string $method, string $uri = '', array $options = []): ResponseInterface;
 
     /**
      * Async request the remote $uri by a HTTP $method verb
@@ -27,6 +40,6 @@ interface ClientDecoratorInterface
      *
      * @return PromiseInterface The `GuzzleHttp\Promise\PromiseInterface` instance
      */
-    public function requestAsync(string $method, string $uri, array $options = []): PromiseInterface;
+    public function requestAsync(string $method, string $uri = '', array $options = []): PromiseInterface;
 
 }
