29a39157bdb9e43ab530d38be5f3f1be40c6ae01
jundayw <jundayw@126.com>
2024-08-27 23:09:11 +0800

    v1.5.0

--- src/Concerns/ChainableTrait.php
+++ src/Concerns/ChainableTrait.php
@@ -115,6 +115,23 @@ trait ChainableTrait
         return $this->getClient()->request('DELETE', $this->pathname(), $options);
     }
 
+    /**
+     * Create and send an HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well.
+     *
+     * @param string $method  HTTP method.
+     * @param array  $options Request options to apply.
+     *
+     * @return ResponseInterface
+     */
+    public function request(string $method, array $options = []): ResponseInterface
+    {
+        return $this->getClient()->request($method, $this->pathname(), $options);
+    }
+
 
     /**
      * Create and send an asynchronous HTTP GET request.
@@ -216,6 +233,24 @@ trait ChainableTrait
     public function deleteAsync(array $options = []): PromiseInterface
     {
         return $this->getClient()->requestAsync('DELETE', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well. Use an array to provide a URL
+     * template and additional variables to use in the URL template expansion.
+     *
+     * @param string $method  HTTP method.
+     * @param array  $options Request options to apply.
+     *
+     * @return PromiseInterface
+     */
+    public function requestAsync(string $method, array $options = []): PromiseInterface
+    {
+        return $this->getClient()->requestAsync($method, $this->pathname(), $options);
     }
 
 }
--- src/Contracts/ChainableInterface.php
+++ src/Contracts/ChainableInterface.php
@@ -125,6 +125,20 @@ interface ChainableInterface
      */
     public function delete(array $options = []): ResponseInterface;
 
+    /**
+     * Create and send an HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well.
+     *
+     * @param string $method  HTTP method.
+     * @param array  $options Request options to apply.
+     *
+     * @return ResponseInterface
+     */
+    public function request(string $method, array $options = []): ResponseInterface;
+
 
     /**
      * Create and send an asynchronous HTTP GET request.
@@ -209,5 +223,20 @@ interface ChainableInterface
      * @return PromiseInterface
      */
     public function deleteAsync(array $options = []): PromiseInterface;
+
+    /**
+     * Create and send an asynchronous HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well. Use an array to provide a URL
+     * template and additional variables to use in the URL template expansion.
+     *
+     * @param string $method  HTTP method.
+     * @param array  $options Request options to apply.
+     *
+     * @return PromiseInterface
+     */
+    public function requestAsync(string $method, array $options = []): PromiseInterface;
 
 }
